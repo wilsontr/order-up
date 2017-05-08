752950614c512d7686ec0e8486c407da3fd5fb38
Trevor Wilson <wilsontr@gmail.com>
2017-05-08 13:39:35 -0400

    Get rid of border on help modal. Update readme

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -18,13 +18,14 @@ const container = css({
 });
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    border: 'none'
   }
 };
 
